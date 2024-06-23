1675f504615a1d15624b6ef9479ad7d44197ca94
moritzsommer <moritz.sommer@rwth-aachen.de>
2024-06-23 02:40:46 +0200

    Adapt package description

--- setup.py
+++ setup.py
@@ -5,8 +5,12 @@ with open("README.md", "r") as fh:
 
 setup(
     name='irdi-parser',
-    version='1.0.0',
+    version='1.0.1',
     description='A parser for International Registration Data Identifier (IRDI) strings.',
+    long_description='The IRDI Parser is a Python program designed to parse IRDI (International Registration Data '
+                     'Identifier) strings based on the international standards ISO/IEC 11179-6, ISO 29002 and ISO/IEC '
+                     '6523. It is used in the ECLASS standard. The program uses regular expressions to extract '
+                     'various components of an IRDI string and returns them in a structured format.',
     author="Moritz Sommer",
     author_email="moritz.sommer@rwth-aachen.de",
     url='https://github.com/moritzsommer/irdi-parser',
